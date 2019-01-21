7c484eb54021d8b719db2c9044cd6df88d8debac
민상 김 <tony9402@naver.com>
2019-01-21 16:38:36 +0900

    2579

--- 2500/2579.cpp
+++ 2500/2579.cpp
@@ -1,37 +1,31 @@
-#include<cstdio>
 #include<iostream>
-#include<vector>
 #include<algorithm>
 
 using namespace std;
 
-vector<int> vc;
-int dp[301][2] = { 0 };
+int input[1000];
+int dp[1000][2];
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int n;
 	cin >> n;
-	vc.resize(n + 1);
-	for (int i = 1; i <= n; i++)
-	{
-		scanf("%d", &vc[i]);
-	}
+	for (int i = 1; i <= n; i++)cin >> input[i];
 
 	for (int i = 1; i <= n; i++)
-	{
 		if (i < 3)
 		{
-			dp[i][0] = vc[i];
-			dp[i][1] = dp[i - 1][0] + vc[i];
+			dp[i][0] = input[i];
+			dp[i][1] = dp[i - 1][0] + input[i];
 		}
 		else
 		{
-			dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + vc[i];
-			dp[i][1] = dp[i - 1][0] + vc[i];
+			dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + input[i];
+			dp[i][1] = dp[i - 1][0] + input[i];
 		}
-	}
 
-	printf("%d", max(dp[n][0], dp[n][1]));
+	cout << max(dp[n][0], dp[n][1]);
 	return 0;
 }
