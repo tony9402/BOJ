8f18fe50dbcca433299851e0443fed71a367ed19
민상 김 <tony9402@naver.com>
2019-01-21 13:45:49 +0900

    1463

--- 1400/1463.cpp
+++ 1400/1463.cpp
@@ -1,35 +1,25 @@
 #include<iostream>
-#include<cstdio>
-
-#define min(x,y) (x)>(y)?(y):(x)
+#include<algorithm>
 
 using namespace std;
+typedef long long ll;
 
-int dp[1000001] = { 0 };
-const int INF = 0x3f3f3f3f;
+ll dp[2000000];
+const ll inf = 0x3ffffffffffff;
 
 int main()
 {
-	int n;
+	ll n;
+	fill(dp, dp + 2000000, inf);
 	cin >> n;
-	fill(dp, dp + n + 1, INF);
 	dp[n] = 0;
 	for (int i = n; i > 0; i--)
 	{
-		if (dp[i] == INF)continue;
-		if (i % 3 == 0)
-		{
-			dp[i / 3] = min(dp[i / 3], dp[i] + 1);
-		}
-		if (i % 2 == 0)
-		{
-			dp[i / 2] = min(dp[i / 2], dp[i] + 1);
-		}
-		if (i >= 2)
-		{
-			dp[i - 1] = min(dp[i - 1], dp[i] + 1);
-		}
+		if (dp[i] == inf)continue;
+		if (i % 3 == 0)dp[i / 3] = min(dp[i / 3], dp[i] + 1);
+		if (i % 2 == 0)dp[i / 2] = min(dp[i / 2], dp[i] + 1);
+		if (i >= 2)dp[i - 1] = min(dp[i - 1], dp[i] + 1);
 	}
-	printf("%d", dp[1]);
+	cout << dp[1];
 	return 0;
 }
