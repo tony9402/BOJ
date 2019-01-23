61810c1003ce4590bec6c196bd40aec0232c076f
민상 김 <tony9402@naver.com>
2019-01-23 15:14:03 +0900

    11051

---
+++ 11000/11051.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+int dp[1001][1001];
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= min(i, k); j++)
+		{
+			if (i == j || j == 0)dp[i][j] = 1;
+			else dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % 10007;
+		}
+	}
+	cout << dp[n][k];
+	return 0;
+}
+
