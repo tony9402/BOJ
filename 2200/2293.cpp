82444edea5b3d0f672845c9089673b420af86027
민상 김 <tony9402@naver.com>
2019-01-26 01:24:03 +0900

    2293

--- 2200/2293.cpp
+++ 2200/2293.cpp
@@ -1,19 +1,12 @@
 #include<iostream>
-#include<cstring>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
-
+int dp[10001];
 vector<int> list;
-int dp[20000];
-const int INF = 0x3f3f3f3f;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	memset(dp, 0x3f, sizeof(dp));
 	int n, k;
 	cin >> n >> k;
 	for (int i = 0; i < n; i++)
@@ -22,21 +15,16 @@ int main()
 		cin >> input;
 		list.push_back(input);
 	}
-	sort(list.begin(), list.end());
-
-	dp[0] = 0;
-	for (int i = 0; i <= 10000; i++)
+	for (int i = 0; i < list.size(); i++)
 	{
-		if (dp[i] == INF)continue;
-		for (auto j : list)
+		if (list[i] > k)continue;
+		dp[list[i]]++;
+		for (int j = list[i] + 1; j <= k; j++)
 		{
-			if (i + j > 10000)continue;
-			dp[i + j] = min(dp[i + j], dp[i] + 1);
+			if (j - list[i] < 0 || j - list[i] > k)continue;
+			dp[j] += dp[j - list[i]];
 		}
 	}
-
-	if (dp[k] == INF)cout << -1;
-	else cout << dp[k];
-
+	cout << dp[k];
 	return 0;
 }
