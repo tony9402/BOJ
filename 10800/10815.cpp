b72870a73188f18314dd9f363ab32f37086e2213
민상 김 <tony9402@naver.com>
2019-01-28 13:44:25 +0900

    10815

---
+++ 10800/10815.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> list;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int input;
+		cin >> input;
+		list.push_back(input);
+	}
+	sort(list.begin(), list.end());
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int input;
+		cin >> input;
+		cout << binary_search(list.begin(), list.end(), input) << ' ';
+	}
+	return 0;
+}
