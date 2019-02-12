70b5a2b97bacd8deba941a4cd84c285ec11d0b57
민상 김 <tony9402@naver.com>
2019-02-12 13:17:03 +0900

    1922

---
+++ 1900/1922.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<tuple>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+
+#define GET(x,a) get<a>(x)
+#define T tuple<int,int,int>
+#define mt make_tuple
+
+using namespace std;
+
+vector<T> vc;
+int tree[10001];
+
+int find(int a)
+{
+	return tree[a] < 0 ? a : find(tree[a]);
+}
+
+bool merge(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+	if (a==b)return false;
+	tree[b] = a;
+	return true;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	memset(tree, -1, sizeof(tree));
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		vc.push_back(mt(c, a, b));
+	}
+
+	sort(vc.begin(), vc.end());
+
+	int ans = 0, cnt = 0;
+	for (auto &i : vc)
+	{
+		int a = GET(i, 1);
+		int b = GET(i, 2);
+		int c = GET(i, 0);
+		if (merge(a, b))
+		{
+			ans += c;
+			if (++cnt == n - 1)break;
+		}
+	}
+	cout << ans;
+	return 0;
+}
